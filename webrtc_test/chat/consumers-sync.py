e195e3145a4a00f49bdc01eeea82a1b3b375ee01
이준혁 <jh_lee@g.kmou.ac.kr>
2021-09-06 22:55:47 +0900

    [ADD] rtctest code

---
+++ webrtc_test/chat/consumers-sync.py
@@ -0,0 +1,4 @@
+from asgiref.sync import async_to_sync
+from channels.generic.websocket import WebsocketConsumer
+import json
+
---
+++ webrtc_test/chat/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.utils.safestring import mark_safe
+import json
+
+# Create your views here.
+
