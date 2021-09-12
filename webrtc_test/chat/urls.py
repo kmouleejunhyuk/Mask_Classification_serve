ddfdfd004c4a2652eb07cb7f0b7b42e93361deae
mickeyshoes <parody9c@gmail.com>
2021-09-12 11:13:58 +0900

    [TEST] operation test

--- webrtc_test/chat/urls.py
+++ webrtc_test/chat/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 
-app_name = 'chat'
+# app_name = 'chat'
 
--- webrtc_test/webrtc/settings.py
+++ webrtc_test/webrtc/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
 	'django.contrib.messages',
 	'django.contrib.staticfiles',
 	'home',
-	'chat',
+	# 'chat',
 	'channels',
 ]
 
